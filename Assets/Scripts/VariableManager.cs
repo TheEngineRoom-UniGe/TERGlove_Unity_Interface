25d3f0d274d4a61b141ab4f63766f05d4d7b626f
Valerio Belcamino <ValerioBelcamino@gmail.com>
2023-03-01 12:39:40 +0100

    Added a Variable Manager

---
+++ Assets/Scripts/VariableManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableManager : MonoBehaviour
+{
+    public int port_number = 2395;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
